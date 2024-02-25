5426fb3a98186f73ab074aa0e934f6bbc49b9816
rsc568 <159699629+rsc568@users.noreply.github.com>
2024-02-25 23:03:57 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -9,7 +9,7 @@ class Config(object):
   SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "shareus.io")
   SHORTLINK_API = os.environ.get('SHORTLINK_API', "8hrMk4EmDPPayaqf8q4zFqzyiTC2")
   BOT_OWNER = int(os.environ.get("BOT_OWNER", "5206100303"))
-  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://rscreations56:1PxiTZtaiHlvrwzf@cluster0.2noxv5y.mongodb.net/?retryWrites=true&w=majority")
+  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://rscreations56:1PxiTZtaiHlvrwzf@cluster0.2noxv5y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "1001791469328")
   LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "1001991440136"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
